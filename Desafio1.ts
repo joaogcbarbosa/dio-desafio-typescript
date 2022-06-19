b19cee8898ce427cced2eda65987b46f5ca71040
João Gabriel Barbosa <joaogcb29@gmail.com>
2022-06-19 20:36:37 -0300

    Exercícios

---
+++ Desafio1.ts
@@ -0,0 +1,16 @@
+/* Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+let employee = {};
+employee.code = 10;
+employee.name = "John";
+*/
+
+//Resposta:
+
+let employee = {
+    firstName: "João",
+    lastName: "Barbosa",
+    age: 23,
+    height: 1.72,
+    weight: 70
+};
